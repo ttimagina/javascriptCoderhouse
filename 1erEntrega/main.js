284cdd8cd22506a99de9fa8cb0a5c37074237819
ttimagina <martinfrette@gmail.com>
2022-05-31 19:50:57 -0300

    agregado de tp1 no obligatorio

--- 1erEntrega/main.js
+++ 1erEntrega/main.js
@@ -1,7 +1,16 @@
-let calcular = parseInt(prompt("- Ingresa tu monto para saber el descuento \n - Ingresa SALIR para terminar"));
+let nombre = prompt("Carga tu nombre");
+
+if (nombre != ""){
+    alert("Hola "+nombre);
+} else {
+    alert("No cargaste tu nombre");
+}
+    
+
+let calcular = parseInt(prompt("- Ingresa tu monto para saber el 20% de descuento \n - Ingresa SALIR para terminar"));
 
 while (calcular != "SALIR"){
     console.log(calcular * 0.20);
     // CONDICION DE SALIDA
-    calcular = prompt("- Ingresa tu monto para saber el descuento \n - Ingresa SALIR para terminar");
+    calcular = prompt("- Ingresa tu monto para saber el 20% de descuento \n - Ingresa SALIR para terminar");
 }
---
+++ clase4-js/main.js
@@ -0,0 +1,70 @@
+/* function saludar(){
+    console.log("Hola Amigo");
+}
+
+saludar(); */
+
+
+// funciones- Declaracion
+/* function solicitarNombre(){
+    //ejecucion
+    let nombreIngresado = prompt("Ingres tu nombre");
+    alert("Hola "+ nombreIngresado);
+}
+// llamado a la funcion
+solicitarNombre();
+
+
+for (let i = 0; i <=3; i++){
+    solicitarNombre();
+}
+
+ */
+// funcion con parametros
+/* 
+function conParametros(parametros1, parametros2){
+    //ejecucion
+    console.log("hola "+parametros1 + parametros2);
+}
+
+conParametros("Daniel","Martin");
+ */
+/* let nombreIngresado = prompt("ingresa tu nombres");
+
+function saludar(nombre){
+    console.log("hola "+ nombre);
+}
+//// le estoy pasando un argumento
+saludar(nombreIngresado);
+saludar("nombre Ingresado String"); */
+
+/* let resultado=0;
+
+function sumar (n1,n2){
+    resultado = n1 + n2;
+}
+function mostrar(mensaje){
+    alert(mensaje);
+}
+
+mostrar("Hola mundo")
+sumar(23,34);
+mostrar(resultado);
+*/
+
+/* 
+function sumar (n1,n2){
+    return  n1 + n2;
+}
+function mostrar(mensaje){
+    alert(mensaje);
+}
+
+let resultado = sumar(15,58);
+
+mostrar(resultado);
+
+mostrar(sumar(12,8)); */
+
+
+//Scope o ambito
