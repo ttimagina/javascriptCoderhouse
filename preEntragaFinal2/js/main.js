a936f1ddc6d92daea7772500abd5c3fce115874b
ttimagina <martinfrette@gmail.com>
2022-06-29 23:34:23 -0300

    me da error al subir

--- preEntragaFinal2/js/main.js
+++ preEntragaFinal2/js/main.js
@@ -12,31 +12,34 @@ saludos.innerHTML = "Hola "+ nombreusuario;
 
 mostrarServicios(listadoDeServicios)
 
-function mostrarServicios(){
-   listadoDeServicios.forEach(el => {
+function mostrarServicios(array){
 
-    let div = document.createElement('div')
-    div.className = 'col-md-3 d-flex flex-column my-3 mx-3'
-    div.innerHTML = `<div>
-    <img src="${el.img}" class="card-img-top img-fluid img_borde_redondo1" alt="imagen">
-    <div class="card-text mb-auto">    
-      <h4 class="titulos_notas--black">${el.nombre}</h4>
-      <p class="card-text txt_justificado">${el.detalle}</p>
-    </div>
-    <div class="bg-branding p-1 font_oswald">
-        <h6>Precio: $ ${el.precio}</h6>
-        <button type="button" id="boton${el.id}" class="btn btn-success">AGREGAR</button>
-    </div> 
-    </div> `
+    for (const el of array) {
 
-    listDisenioMostrar.appendChild(div)
-
-    let btnAgregar = document.getElementById(`boton${el.id}`)
+        listDisenioMostrar.innerHTML = ""
         
-    btnAgregar.addEventListener('click',()=>{
+        let div = document.createElement('div')
+        div.className = 'col-md-3 d-flex flex-column my-3 mx-3'
+        div.innerHTML = `<div>
+        <img src="${el.img}" class="card-img-top img-fluid img_borde_redondo1" alt="imagen">
+        <div class="card-text mb-auto">    
+          <h4 class="titulos_notas--black">${el.nombre}</h4>
+          <p class="card-text txt_justificado">${el.detalle}</p>
+        </div>
+        <div class="bg-branding p-1 font_oswald">
+            <h6>Precio: $ ${el.precio}</h6>
+            <button type="button" id="boton${el.id}" class="btn btn-success">AGREGAR</button>
+        </div> 
+        </div> `
+
+        listDisenioMostrar.appendChild(div)
+
+        let btnAgregar = document.getElementById(`boton${el.id}`)
+        
+        btnAgregar.addEventListener('click',()=>{
             agregarPresupuesto(el.id);
-    })
-})
+        })
+    }
 }
 
  function agregarPresupuesto(id) {
@@ -62,16 +65,33 @@ function mostrarServicios(){
 function mostrarServicios(servicioAgregar){
 
     let div = document.createElement('div')
-    div.innerHTML = `<div>
-        <div class="card-text mb-auto">    
-            <h4 class="titulos_notas--black"> ${servicioAgregar.nombre}</h4>
-            
+    div.innerHTML = `<div class="card-text mb-auto">    
+            <h4 class="titulos_notas--black">${servicioAgregar.nombre}</h4>
         </div>
         <div class="bg-branding p-1 font_oswald">
             <h6>Precio: $ ${servicioAgregar.precio}</h6>
-        </div>    
-    </div> `
+        </div> 
+        <p id="cantidad${servicioAgregar.id}">cantidad: ${servicioAgregar.cantidad}</p>
+        <button id="eliminar${servicioAgregar.id}" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>`
 
     mostrarServiciosAgregados.appendChild(div)
 
+    let btnEliminar= document.getElementById(`eliminar${servicioAgregar.id}`)
+    btnEliminar.addEventListener('click', ()=>{
+        if(servicioAgregar.cantidad ==1){
+            btnEliminar.parentElement.remove()
+            carritoServicios = carritoServicios.filter(item => item.id !== servicioAgregar.id)
+            actualizarCarrito()
+        }else{
+            servicioAgregar.cantidad = servicioAgregar.cantidad -1 
+        document.getElementById(`cantidad${servicioAgregar.id}`).innerHTML = `<p id="cantidad${servicioAgregar.id}">cantidad: ${servicioAgregar.cantidad}</p>`
+        actualizarCarrito()
+        }
+    
+    })
 }
+
+function  actualizarCarrito (){
+    contadorServicio.innerText = carritoServicios.reduce((acc,el)=> acc + el.cantidad, 0)
+    precioTotal.innerText = carritoServicios.reduce((acc,el)=> acc + (el.precio * el.cantidad) , 0)                                                            
+ }
